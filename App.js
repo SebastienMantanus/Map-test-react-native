9ea72fc85022c354b8c532d5e90d00a79d595e32
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2023-03-04 15:57:35 +0100

    Marker fix

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ export default function App() {
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: 48.856614,
           longitude: 2.3522219,
+          latitude: 48.856614,
           latitudeDelta: 0.2,
           longitudeDelta: 0.2,
         }}
       >
-        <MapView.Marker
+        <Marker
           coordinate={{
             longitude: longitude,
             latitude: latitude,
